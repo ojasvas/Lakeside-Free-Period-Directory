89ed23b31cdf658dd2ccf20b342ac87c56fccee9
Ethan Du <ethand22@lakesideschool.org>
2021-02-10 10:49:46 -0800

    First commit on borrowed computer

---
+++ testUI/Shared/testUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  testUIApp.swift
+//  Shared
+//
+//  Created by Student on 2/10/21.
+//
+
+import SwiftUI
+
+@main
+struct testUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
