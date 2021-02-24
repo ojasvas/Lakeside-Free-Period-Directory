c830d41c3cda0c5fbf4f6a61b43da24e80c7f1bf
Ethan Du <ethand22@lakesideschool.org>
2021-02-24 15:00:38 -0800

    Finished sign up/log in screens

--- Free Period Directory/Free Period Directory/View Controllers/Login ViewController.swift
+++ Free Period Directory/Free Period Directory/View Controllers/Login ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class Login_ViewController: UIViewController {
 
@@ -30,18 +31,67 @@ class Login_ViewController: UIViewController {
         errorLabel.alpha = 0
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // Check the field and validate that the data is correct.
+    // If everything is correct, nil is returned. Otherwise and error message is returned.
+    func validateFields() -> String? {
+        
+        // Check to see if all fields are filled in
+        if emailTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+            passwordTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" {
+            
+            return "Please fill in all the fields"
+        }
+        
+        // !!!!!!!!!! TODO !!!!!!!!!! Call email validation method here!
+        
+        // Check to see if the password is secure
+        let cleanedPassword = passwordTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        if isPasswordValid(cleanedPassword) == false {
+            
+            // Password is not secure
+            return "Please make sure your password is at least 8 characters, contains a special character, and a number"
+        }
+        
+        return nil
     }
-    */
-
+    
+    // !!!!!!!!!! TODO !!!!!!!!!! We should figure out how to validate lakeside emails
+    
+    // Checks to see if the password is valid
+    func isPasswordValid(_ password : String) -> Bool{
+        
+        let passwordTest = NSPredicate(format: "SELF MATCHES %@", "^(?=.*[a-z])(?=.*[$@$#!%*?&])[A-Za-z\\d$@$#!%*?&]{8,}")
+        return passwordTest.evaluate(with: password)
+    }
+    
     @IBAction func loginTapped(_ sender: Any) {
+
+        // Create cleaned versions of text fields
+        let email = emailTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        let password = passwordTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        // Logging in the user
+        Auth.auth().signIn(withEmail: email, password: password) { (result, error) in
+            
+            if error != nil {
+                
+                // Could not sign in
+                self.errorLabel.text = "Email or password was incorrect"
+                self.errorLabel.alpha = 1
+            } else {
+                
+                self.goToHomescreen()
+            }
+        }
     }
     
-    
+    func goToHomescreen() {
+        
+        let homeViewController =
+            storyboard?.instantiateViewController(identifier: Constants.Storyboard.homeViewController) as? Home_ViewController
+        
+        view.window?.rootViewController = homeViewController
+        view.window?.makeKeyAndVisible()
+    }
 }
