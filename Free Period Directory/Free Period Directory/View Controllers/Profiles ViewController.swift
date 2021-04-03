a9c6b2ed392f9c4b06a02b97c2a48449f708e2c4
Ethan Du <ethand22@lakesideschool.org>
2021-04-02 21:14:46 -0700

    fixed password field bug

---
+++ Free Period Directory/Free Period Directory/View Controllers/Profiles ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  Profiles ViewController.swift
+//  Free Period Directory
+//
+//  Created by Student on 4/2/21.
+//
+
+import UIKit
+
+class Profiles_ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
