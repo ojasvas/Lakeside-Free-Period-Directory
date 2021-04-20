622289491bd3742e76554356f26b6edd23fc0611
Allise Thurman <alliset21@lakesideschool.org>
2021-04-20 14:00:25 -0700

    Class Schedule Signup Screen

---
+++ Free Period Directory/Free Period Directory/View Controllers/Signup ViewControllers/CourseCell.swift
@@ -0,0 +1,23 @@
+//
+//  CourseCell.swift
+//  Free Period Directory
+//
+//  Created by Allise Thurman on 4/6/21.
+//
+
+import UIKit
+
+class CourseCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Free Period Directory/Free Period Directory/View Controllers/Signup ViewControllers/Study Spot ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  Study Spot ViewController.swift
+//  Free Period Directory
+//
+//  Created by Allise Thurman on 4/17/21.
+//
+
+import UIKit
+
+class Study_Spot_ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
