176d69563f2c99d8d78c69faf867699cd82747e8
Ethan Du <ethand22@lakesideschool.org>
2021-05-26 14:20:42 -0700

    commented profiles view controller

--- Free Period Directory/Free Period Directory/View Controllers/Profiles ViewController.swift
+++ Free Period Directory/Free Period Directory/View Controllers/Profiles ViewController.swift
@@ -20,27 +20,38 @@ class Profiles_ViewController: UIViewController {
     
     @IBOutlet weak var searchButton: UIButton!
     
+    // create an array of User Profile objects
     var userProfiles: [UserProfile] = []
+    // create an array of stackViews (the profiles themselves)
     var profileStacks: [UIStackView] = []
+    // this array is used to get all of the data from the database
     var data: [dataType] = []
+    // this array will contain the data (users) that match the keyword search
     var filteredData: [dataType] = []
-    var filteredProfiles: [String] = []
+    // this array will contain the user ids of the users that match the keyword search
+    var filteredUsers: [String] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        // get all data from database
         self.getData()
         
+        // add scroll functionality
         view.addSubview(scrollView)
+        // create a vertical stack in the scrollView to help arrange the profiles
         self.scrollView.addSubview(vertStack)
         
+        // set up vertical stack
         self.vertStack.translatesAutoresizingMaskIntoConstraints = false
         self.vertStack.spacing = 10
         self.vertStack.alignment = .fill
         self.vertStack.distribution = .fillEqually
         
+        // get all uids
         self.getDocNames() { (data) in
             let allUsers = data
             var i = 0
+            // loop that creates all the profiles and adds them to the vertical stack
             while i < allUsers.count {
                 let userProfile = UserProfile(uid: allUsers[i])
                 self.userProfiles.append(userProfile)
@@ -49,32 +60,42 @@ class Profiles_ViewController: UIViewController {
                 self.vertStack.addArrangedSubview(profile)
                 i = i + 1
             }
+            // set size of the scrollView
             self.scrollView.contentSize = CGSize(width: self.view.frame.size.width, height: CGFloat((allUsers.count) * 440))
         }
     }
 
     @IBAction func searchPressed(_ sender: Any) {
+        // remove everything from the vertical stack
         self.removeStackViews()
+        
+        // make the searched text uniform
         var searchedText = searchTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
         searchedText = searchedText.lowercased()
+        
+        // make sure these two arrays are empty each time the search button is pressed
         filteredData.removeAll()
-        filteredProfiles.removeAll()
+        filteredUsers.removeAll()
 
+        // if anything in data includes the searched text, add to the filteredData array
         for i in data {
             if i.allText.lowercased().contains(searchedText){
                 filteredData.append(i)
             }
         }
+        
+        // create profiles for the filteredData users and add to the vertical stack
         for i in filteredData {
             let userProfile = UserProfile(uid: i.id)
             let profile = userProfile.createProfile(view: self.vertStack)
             self.vertStack.addArrangedSubview(profile)
-            filteredProfiles.append(i.id)
+            filteredUsers.append(i.id)
         }
-        self.scrollView.contentSize = CGSize(width: self.view.frame.size.width, height: CGFloat((filteredProfiles.count) * 440))
-        
+        // resize the scrollView
+        self.scrollView.contentSize = CGSize(width: self.view.frame.size.width, height: CGFloat((filteredUsers.count) * 440))
+
+        // Send alert if no profiles match the keyword the user entered
         if filteredData.count == 0 {
-            // Send alert if no profiles match the keyword the user entered
             // Source: developer.apple.com
             let errorAlert = UIAlertController(title: "Error!", message: "No profiles match your keyword", preferredStyle: .alert)
             errorAlert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: { _ in
@@ -84,7 +105,7 @@ class Profiles_ViewController: UIViewController {
         }
     }
     
-    
+    // function that gets all the uids from the database except the current user's
     func getDocNames(completion: @escaping (Array<String>) -> Void) {
         var docIDArray: [String] = []
         let db = Firestore.firestore()
@@ -104,7 +125,7 @@ class Profiles_ViewController: UIViewController {
         }
     }
     
-    //Gets the userID of the current user
+    // Gets the userID of the current user
     func getCurrentUserID() -> Any? {
         let user = Auth.auth().currentUser
         let uid = user?.uid
@@ -117,7 +138,7 @@ class Profiles_ViewController: UIViewController {
         }
     }
     
-    //Gets the data from the entire courses database and appends it to the courses and data arrays
+    // Gets the data from the database and organizes it
     func getData() {
         let db = Firestore.firestore()
         db.collection("users").getDocuments{[weak self] snap, err in
@@ -145,6 +166,7 @@ class Profiles_ViewController: UIViewController {
                     let interestTwo = i["interest2"] as! String
                     let interestThree = i["interest3"] as! String
                     let studySpot = i["favoriteStudySpot"] as! String
+                    // allText will be used to determine if the keyword can be found in a user's profile
                     let allText = ("\(firstName) \(lastName) \(courseOne) \(courseTwo) \(courseThree) \(courseFour) \(courseFive) \(courseSix) \(courseSeven) \(freeOne) \(freeTwo) \(interestOne) \(interestTwo) \(interestTwo) \(studySpot)")
                     self?.data.append(dataType(id: id, firstName: firstName, lastName: lastName, courseOne: courseOne, courseTwo: courseTwo, courseThree: courseThree, courseFour: courseFour, courseFive: courseFive, courseSix: courseSix, courseSeven: courseSeven, freeOne: freeOne, freeTwo: freeTwo, interestOne: interestOne, interstTwo: interestTwo, interestThree: interestThree, studySpot: studySpot, allText: allText))
                 }
@@ -173,6 +195,7 @@ class Profiles_ViewController: UIViewController {
         var allText: String
     }
     
+    // removes all views from a stack
     func removeStackViews() {
         let profileViews = vertStack.subviews
         for subUIView in profileViews {
